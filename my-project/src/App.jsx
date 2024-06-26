05cd41eb42030967768f9905e5595b393464c7b0
Javier Gonzalez <javgon2306@gmail.com>
2024-06-25 22:29:13 -0300

    changes_03

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
+
 const TourismCard = ({ image, title, rating, days, nights }) => (
+
   <>
-  <div className="relative max-w-sm rounded overflow-hidden shadow-lg m-4 group">
-    <img className="w-full" src={image} alt={title} />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2 text-center">{title}</div>
-    </div>
-    <div className="absolute inset-0 bg-gray-900 bg-opacity-75 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white p-4">
-      <div className="text-lg font-bold">{title}</div>
-      <div className="mt-2">
-        <span className="block text-sm">Rating: {rating}</span>
-        <span className="block text-sm">Days: {days}</span>
-        <span className="block text-sm">Nights: {nights}</span>
+    <div className="relative max-w-sm rounded overflow-hidden shadow-lg m-4 group">
+      <img className="w-full" src={image} alt={title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2 text-center">{title}</div>
+      </div>
+      <div className="absolute inset-0 bg-gray-900 bg-opacity-75 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white p-4">
+        <div className="text-lg font-bold">{title}</div>
+        <div className="mt-2">
+          <span className="block text-sm">Rating: {rating}</span>
+          <span className="block text-sm">Days: {days}</span>
+          <span className="block text-sm">Nights: {nights}</span>
+        </div>
       </div>
     </div>
-  </div>
   </>
+
 );
+
 
 
 const App = () => {
